7fb682dfe9cc94da382ab6fee0d48d459b12fddc
akabeera <avenash.kabeera01@gmail.com>
2024-03-13 10:45:46 -0400

    feat: update config and add check in crypto-bot

--- crypto_bot.py
+++ crypto_bot.py
@@ -152,6 +152,10 @@ class CryptoBot:
             if ticker_info is None:
                 logger.error(f"{ticker_pair}: unable to fetch ticker info, skipping")
                 continue
+            
+            if "bid" not in ticker_info or ticker_info["bid"] is None:
+                logger.error(f"{ticker_pair}: missing or None bid_price, skipping")
+                continue
 
             take_profit_threshold = self.take_profit_threshold
             take_profit_evaluation_type = self.take_profit_evaluation_type
@@ -159,9 +163,7 @@ class CryptoBot:
             if ticker_pair in self.overrides and CONSTANTS.CONFIG_TAKE_PROFITS in self.overrides[ticker_pair]:
                 (take_profit_threshold, take_profit_evaluation_type) = self.init_take_profits_config(self.overrides[ticker_pair][CONSTANTS.CONFIG_TAKE_PROFITS])
 
-            if "bid" not in ticker_info or ticker_info["bid"] is None:
-                logger.error(f"{ticker_pair}: missing or None bid_price, skipping")
-                continue
+
 
             profitable_positions_to_exit = find_profitable_trades(ticker_pair, 
                                                                   avg_position, 
