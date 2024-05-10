d08ed59ea1c4608fd8555db285e18818da49c0bc
Avenash Kabeera <avenash.kabeera@gmail.com>
2024-05-10 11:02:04 +0800

    feat: parameterized log filename

--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,8 @@ import os
 import logging
 import logging.config
 
+from datetime import datetime
+
 logger = logging.getLogger(__name__)
 
 def configureLogger(logLevel: str):
@@ -11,7 +13,9 @@ def configureLogger(logLevel: str):
     if not os.path.exists(log_directory):
         os.makedirs(log_directory)
 
-    logging.config.fileConfig("./config/logging.conf", disable_existing_loggers=False)
+    log_filename = f"{log_directory}/crypto_bot_{datetime.now().strftime('%Y%m%d')}.log"
+
+    logging.config.fileConfig("./config/logging.conf", defaults={'logfilename': log_filename}, disable_existing_loggers=False)
 
     if not logLevel: 
         return
